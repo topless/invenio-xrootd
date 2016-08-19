a8fda9c98cca3e508aee4ad6520ad02401e24052
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2016-08-19 10:06:51 +0200

    release: v1.0.0a1
    Signed-off-by: Lars Holm Nielsen <lars.holm.nielsen@cern.ch>

--- CHANGES.rst
+++ CHANGES.rst
@@ -25,6 +25,6 @@
 Changes
 =======
 
-Version 1.0.0 (released TBD)
+Version 1.0.0a1 (released 2016-08-19)
 
 - Initial public release.
--- setup.py
+++ setup.py
@@ -101,10 +101,6 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Development Status :: 3 - Alpha',
     ],
 )
